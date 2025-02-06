2c657d60f0d9aa7c4a4f288e9d0be0e5a1512077
Asim Razzaq <147363287+AsimRazzaq01@users.noreply.github.com>
2025-02-05 19:29:22 -0500

    Create and use a string resource

--- app/src/main/java/com/example/wed_2_5_25/MainScreen.kt
+++ app/src/main/java/com/example/wed_2_5_25/MainScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
@@ -27,7 +28,8 @@ fun MainScreen(modifier: Modifier) {
     Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally ) {
 
         Text(
-            text = "I love Android!",
+            //text = "I love Android!",
+            text = stringResource(id = R.string.app_name),
             fontSize = 40.sp,
             color = Color.Blue,
             fontWeight = FontWeight.Bold
